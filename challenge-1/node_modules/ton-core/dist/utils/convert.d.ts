22dfae15e865985a13246541bf617eddd7364ab5
Coalus <im-coalus@proton.me>
2024-02-19 16:35:54 +0300

    feat: fix NFT-collection

--- challenge-1/node_modules/ton-core/dist/utils/convert.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Whales Corp.
- * All Rights Reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-export declare function toNano(src: number | string | bigint): bigint;
-export declare function fromNano(src: bigint | number | string): string;
--- challenge-1/node_modules/ton/dist/utils/createTestClient.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright (c) Whales Corp.
- * All Rights Reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
-import { TonClient } from "../client/TonClient";
-export declare function createTestClient(net?: 'testnet' | 'mainnet'): TonClient;
